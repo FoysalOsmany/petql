b543cd3de4e6c668a3cad7c7ff300405c7e3bb6e
FoysalOsmany <foysalo@gmail.com>
2019-10-20 16:21:58 +0600

    pet service

---
+++ src/components/pet/pet.service.ts
@@ -0,0 +1,42 @@
+import {IPetService} from "./pet.service.interface";
+import {injectable} from "inversify";
+import {IPet} from "./pet.interface";
+import {db} from "../../utils/db.driver";
+import Q from "q";
+
+@injectable()
+export class PetService implements IPetService {
+  create(petData: IPet): any {
+    return Q.ninvoke(db.collection('pets'), 'insert', petData)
+      .then((data: any) => Q.resolve(data))
+      .catch((e: any) => Q.reject(e));
+  }
+
+  findAll(): any {
+    return Q.ninvoke(db.collection('pets').find({}), 'toArray')
+      .then((data: any) => Q.resolve(data))
+      .catch((e: any) => Q.reject(e));
+  }
+
+  findById(_id: string): any {
+    return Q.ninvoke(db.collection('pets'), 'findOne', {_id})
+      .then((data: any) => Q.resolve(data))
+      .catch((e: any) => Q.reject(e));
+  }
+
+  update(_id: string, petData: IPet): any {
+    return Q.ninvoke(db.collection('pets'), 'update', {_id: _id}, petData)
+      .then((data: any) => Q.resolve(data))
+      .catch((e: any) => {
+        return Q.reject(e)
+      });
+  }
+
+  delete(_id: string): any {
+    return Q.ninvoke(db.collection('pets'), 'remove', {_id: _id})
+      .then(() => Q.resolve('deleted'))
+      .catch((e: any) => {
+        return Q.reject(e)
+      });
+  }
+}
