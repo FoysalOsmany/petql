83a7f07230e15ad44dbbc1bb83a80f098d6fa5c9
FoysalOsmany <foysalo@gmail.com>
2019-10-20 16:21:13 +0600

    owner service

---
+++ src/components/owner/owner.service.ts
@@ -0,0 +1,84 @@
+import {IOwnerService} from "./owner.service.interface";
+import {inject, injectable} from "inversify";
+import {IOwner} from "./owner.interface";
+import {db} from "../../utils/db.driver";
+import Q from "q";
+import {IPet} from "../pet/pet.interface";
+import {TYPES} from "../../configurations/types";
+import {IPetService} from "../pet/pet.service.interface";
+
+@injectable()
+export class OwnerService implements IOwnerService {
+  constructor(
+    @inject(TYPES.IPetService) private petService: IPetService) {
+  }
+
+  create(ownerData: IOwner): any {
+    return Q.ninvoke(db.collection('owners'), 'insert', ownerData)
+      .then((data: any) => Q.resolve(data))
+      .catch((e: any) => Q.reject(e));
+  }
+
+  findAll() {
+    return Q.ninvoke(db.collection('owners').find({}), 'toArray')
+      .then(data => Q.resolve(data))
+      .catch(e => Q.reject(e));
+  }
+
+  findById(_id: string): Q.Promise<any> {
+    return Q.ninvoke(db.collection('owners'), 'findOne', {_id})
+      .then((data) => Q.resolve(<IOwner>data))
+      .catch(e => Q.reject(e));
+  }
+
+  update(_id: string, ownerData: IOwner) {
+    return Q.ninvoke(db.collection('owners'), 'update', {_id: _id}, ownerData)
+      .then(data => Q.resolve(data))
+      .catch(e => {
+        return Q.reject(e)
+      });
+  }
+
+  delete(_id: string) {
+    return Q.ninvoke(db.collection('owners'), 'remove', {_id: _id})
+      .then(() => Q.resolve('deleted'))
+      .catch(e => {
+        return Q.reject(e)
+      });
+  }
+
+  ownPet(_id: string, pid: string): any {
+    return Q.all([
+      this.findById(_id),
+      this.petService.findById(pid)
+    ]).spread((owner: IOwner, pet: IPet) => {
+        const {_id, name} = pet;
+
+        owner['owns'] = owner['owns'] || [];
+
+        if (owner.hasOwnProperty('owns') && owner.owns) {
+          owner.owns = owner.owns.filter((pet: any) => pet._id === pid);
+        }
+
+        owner['owns'].push({_id, name});
+
+        return this.update(_id, owner)
+          .then(data => Q.resolve(data))
+          .catch(e => Q.reject(e))
+      }
+    ).catch(e => Q.reject(e))
+  }
+
+  surrenderPet(_id: string, pid: string): any {
+    return this.findById(_id)
+      .then((owner: IOwner) => {
+        if (owner.hasOwnProperty('owns') && owner.owns) {
+          owner.owns = owner.owns.filter((pet: any) => pet._id === pid);
+        }
+
+        return this.update(_id, owner)
+          .then(data => Q.resolve(data))
+          .catch(e => Q.reject(e))
+      })
+  }
+}
