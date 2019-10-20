ff67c1f6ca6216fb404d8398e17a1699391da5fc
FoysalOsmany <foysalo@gmail.com>
2019-10-20 16:19:33 +0600

    define types

---
+++ src/configurations/types.ts
@@ -0,0 +1,11 @@
+let TYPES = {
+  IComponentRoutes: Symbol('IComponentRoutes'),
+
+  IPetService: Symbol('IPetService'),
+  IOwnerService: Symbol('IOwnerService'),
+
+  IPetRoutes: Symbol('IPetRoutes'),
+  IOwnerRoutes: Symbol('IOwnerRoutes'),
+};
+
+export { TYPES };
